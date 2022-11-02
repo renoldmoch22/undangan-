99f3059ce9d6f78b410adb1e8f29212789451584
mank renold <renoldmoch@gmail.com>
2022-11-03 01:10:24 +0700

    up
    up

--- wp-login.php
+++ wp-login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en-US">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Log Masuk &lsaquo; Undangan &#8212; WordPress</title>
+	<title>Log In &lsaquo; Undangan &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="stylesheet" id="dashicons-css" href="https://renoldmoch22.github.io/undangan-/wp-includes/css/dashicons.min.css?ver=6.1" media="all">
 <link rel="stylesheet" id="buttons-css" href="https://renoldmoch22.github.io/undangan-/wp-includes/css/buttons.min.css?ver=6.1" media="all">
@@ -12,56 +12,56 @@
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
-	<body class="login no-js login-action-login wp-core-ui  locale-id-id">
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
 		<div id="login">
-		<h1><a href="https://wordpress.org/">Diberdayakan dengan WordPress</a></h1>
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
 		<form name="loginform" id="loginform" action="http://localhost/undangan/wp-login.php" method="post">
 			<p>
-				<label for="user_login">Nama Pengguna atau Alamat Email</label>
+				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
 			</p>
 
 			<div class="user-pass-wrap">
-				<label for="user_pass">Sandi</label>
+				<label for="user_pass">Password</label>
 				<div class="wp-pwd">
 					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password">
-					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Tampilkan sandi">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
-						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Ingat Saya</label></p>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log Masuk">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
 									<input type="hidden" name="redirect_to" value="http://localhost/undangan/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-				<a href="https://renoldmoch22.github.io/undangan-/wp-login.php?action=lostpassword">Lupa sandi Anda?</a>			</p>
+				<a href="https://renoldmoch22.github.io/undangan-/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="https://renoldmoch22.github.io/undangan-/">&larr; Pergi ke Undangan</a>		</p>
+			<a href="https://renoldmoch22.github.io/undangan-/">&larr; Go to Undangan</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
 
 					<label for="language-switcher-locales">
 						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
-						<span class="screen-reader-text">Bahasa</span>
+						<span class="screen-reader-text">Language</span>
 					</label>
 
 					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
-<option value="id_ID" lang="id" selected data-installed="1">Bahasa Indonesia</option></select>
+<option value="id_ID" lang="id" data-installed="1">Bahasa Indonesia</option></select>
 					
 					
 					
-						<input type="submit" class="button" value="Ubah">
+						<input type="submit" class="button" value="Change">
 
 					</form>
 				</div>
@@ -76,22 +76,12 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Kekuatan sandi tak diketahui","short":"Sangat lemah","bad":"Lemah","good":"Sedang","strong":"Kuat","mismatch":"Tidak sama"};</script>
-<script id="password-strength-meter-js-translations">( function( domain, translations ) {
-	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
-	localeData[""].domain = domain;
-	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2022-11-02 03:09:22+0000","generator":"GlotPress\/4.0.0-alpha.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"id"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s telah kedaluarsa sejak versi %2$s! Gunakan %3$s sebagai gantinya. Silakan pertimbangkan untuk menulis kode yang lebih inklusif."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
+<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};</script>
 <script src="https://renoldmoch22.github.io/undangan-/wp-admin/js/password-strength-meter.min.js?ver=6.1" id="password-strength-meter-js"></script>
 <script src="https://renoldmoch22.github.io/undangan-/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/undangan\/wp-admin\/admin-ajax.php"}};</script>
 <script src="https://renoldmoch22.github.io/undangan-/wp-includes/js/wp-util.min.js?ver=6.1" id="wp-util-js"></script>
 <script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"93fa09f7c4"};</script>
-<script id="user-profile-js-translations">( function( domain, translations ) {
-	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
-	localeData[""].domain = domain;
-	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2022-11-02 03:09:22+0000","generator":"GlotPress\/4.0.0-alpha.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"id"},"Your new password has not been saved.":["Sandi baru Anda belum disimpan."],"Hide":["Sembunyikan"],"Show":["Tampilkan"],"Confirm use of weak password":["Setujui penggunaan sandi yang lemah"],"Hide password":["Sembunyikan sandi"],"Show password":["Tampilkan sandi"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
 <script src="https://renoldmoch22.github.io/undangan-/wp-admin/js/user-profile.min.js?ver=6.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
